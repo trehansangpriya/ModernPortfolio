a7baf61a4d0c428a2f2a9ccce39d9ab1687a6059
Trehan Sangpriya <trehansangpriya@gmail.com>
2020-12-07 18:01:42 +0530

    update

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const darkL = document.querySelector('.logo-l')
+const darkM = document.querySelector('.logo-m')
+
+darkL.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
+darkM.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
