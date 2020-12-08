0754c4c098af7372475c1eaa6cb72b5cda71b08a
Trehan Sangpriya <trehansangpriya@gmail.com>
2020-12-08 21:35:44 +0530

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,15 @@ darkM.addEventListener('click', () => {
 
 const tl = gsap.timeline({ defaults: { ease: 'slow' } })
 const tl2 = gsap.timeline({ defaults: { ease: 'power4' } })
+const tlScroll = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.about',
+        start: '400px bottom',
+        end: 'top top',
+        markers: true,
+        // scrub: true,
+    }
+})
 tl.to('.illus', { display: 'block', opacity: 1, duration: 3, delay: 1 })
 tl.to('#y-1', { x: '200px', repeat: -1, yoyo: true, duration: .8 }, '-=5')
 tl.to(' #y-2', { x: '150px', repeat: -1, yoyo: true, duration: .5 }, '-=5')
@@ -22,3 +31,19 @@ tl2.from('.hero-img img', { y: '-20%', opacity: 0, duration: 2, delay: 1 })
     .from('nav', { display: 'none', duration: 1, opacity: 0 }, '-=2')
     .from('.top-nav-m', { display: 'none', duration: 1, opacity: 0 }, '-=2')
     .from('.blue', { display: 'none', opacity: 0, duration: 3 }, '-=2')
+
+tlScroll.from('.abt-head', { x: '-100%', opacity: 0, duration: .7 })
+    .from('.abt-content', { y: '50%', opacity: 0, duration: .5 })
+    .from('.know-more', { y: '100%', opacity: 0, duration: .2 })
+
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: '.skills',
+        start: '400px bottom',
+        end: 'top top',
+        markers: true,
+        // scrub: true,
+    }
+}).from('.skills-head', { x: '-100%', opacity: 0, duration: .7 })
+    .from('.card', { x: '-100%', opacity: 0, duration: 0.5 }, '-=0.5')
